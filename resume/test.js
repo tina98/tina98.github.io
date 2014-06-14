e85f6bc9d6ee7ab6674f427cd166f943ee9b0460
tina98 <tinali3698@gmail.com>
2014-06-14 10:53:39 -0400

    Update test.js

--- resume/test.js
+++ resume/test.js
@@ -1,9 +1,9 @@
-// $(function() {
-// 			$("html, body").mousewheel(function(event, delta) {
-// 				this.scrollLeft -= (delta * 30);
-// 				event.preventDefault();
-// 			});
-// 		});
+$(function() {
+			$("html, body").mousewheel(function(event, delta) {
+				this.scrollLeft -= (delta * 30);
+				event.preventDefault();
+			});
+		});
 function work(){
 document.getElementById("doot").innerHTML="wiiiiiirsjdkf";
 }
