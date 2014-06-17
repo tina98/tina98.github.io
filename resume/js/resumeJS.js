6563b943496242f4293eca49cdeb8fbbdadfa8bb
tina98 <tinali3698@gmail.com>
2014-06-17 11:27:19 -0400

    Update resumeJS.js

--- resume/js/resumeJS.js
+++ resume/js/resumeJS.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-    $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-     
-        $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-             
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
- 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
-    });    
+
+  $(window).scroll(function() {
+    drawLine( $('#route'),
+              document.getElementById('path') );
+  });
+  
+  // init the line length
+    drawLine( $('#route'),
+              document.getElementById('path') );
+
+  //draw the line
+  function drawLine(container, line){
+    
+    var pathLength = line.getTotalLength(),
+        maxScrollTop = $(document).height() - $(window).height(),
+        percentDone = $(window).scrollTop() / maxScrollTop,
+        length = percentDone * pathLength;
+    line.style.strokeDasharray = [ length ,pathLength].join(' ');
+  }
+  
 });
