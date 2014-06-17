b2d99f12f721e1c3caab140a8628eb58b2880f37
tina98 <tinali3698@gmail.com>
2014-06-17 11:31:36 -0400

    Update resumeJS.js

--- resume/js/resumeJS.js
+++ resume/js/resumeJS.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-
-  $(window).scroll(function() {
-    drawLine( $('#route'),
-              document.getElementById('path') );
-  });
+$("#hello").html("sdfs");
+  // $(window).scroll(function() {
+  //   drawLine( $('#route'),
+  //             document.getElementById('path') );
+  // });
   
-  // init the line length
-    drawLine( $('#route'),
-              document.getElementById('path') );
+  // // init the line length
+  //   drawLine( $('#route'),
+  //             document.getElementById('path') );
 
-  //draw the line
-  function drawLine(container, line){
+  // //draw the line
+  // function drawLine(container, line){
     
-    var pathLength = line.getTotalLength(),
-        maxScrollTop = $(document).height() - $(window).height(),
-        percentDone = $(window).scrollTop() / maxScrollTop,
-        length = percentDone * pathLength;
-    line.style.strokeDasharray = [ length ,pathLength].join(' ');
-  }
+  //   var pathLength = line.getTotalLength(),
+  //       maxScrollTop = $(document).height() - $(window).height(),
+  //       percentDone = $(window).scrollTop() / maxScrollTop,
+  //       length = percentDone * pathLength;
+  //   line.style.strokeDasharray = [ length ,pathLength].join(' ');
+  // }
   
 });
