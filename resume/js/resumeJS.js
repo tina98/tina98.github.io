383b138fe372b56070070796fea8e2f395342690
tina98 <tinali3698@gmail.com>
2014-06-17 12:00:18 -0400

    Update resumeJS.js

--- resume/js/resumeJS.js
+++ resume/js/resumeJS.js
@@ -1,25 +1,47 @@
+// $(document).ready(function(){
+
+
+//   //draw the line
+//   function drawLine(container, line){
+    
+//     var pathLength = line.getTotalLength();
+//       var  maxScrollTop = $("body").height() - $(window).height();
+//       var  percentDone = $(window).scrollTop() / maxScrollTop;
+//       var length = percentDone * pathLength;
+//     line.style.strokeDashArray = [ length ,pathLength].join(' ');
+//   }
+
+//   $(window).scroll(function() {
+//     drawLine( $('#route'),
+//               $('#path') );
+//   });
+  
+//   // // init the line length
+//   //   drawLine( $('#route'),
+//   //             document.getElementById('path') );
+
+
+  
+// });
 $(document).ready(function(){
 
+  $(window).scroll(function() {
+    drawLine( $('#route'),
+              document.getElementById('path') );
+  });
+  
+  // init the line length
+    drawLine( $('#route'),
+              document.getElementById('path') );
 
   //draw the line
   function drawLine(container, line){
     
-    var pathLength = line.getTotalLength();
-      var  maxScrollTop = $("body").height() - $(window).height();
-      var  percentDone = $(window).scrollTop() / maxScrollTop;
-       var length = percentDone * pathLength;
-    line.style.strokeDashArray = [ length ,pathLength].join(' ');
+    var pathLength = line.getTotalLength(),
+        maxScrollTop = $(document).height() - $(window).height(),
+        percentDone = $(window).scrollTop() / maxScrollTop,
+        length = percentDone * pathLength;
+    line.style.strokeDasharray = [ length ,pathLength].join(' ');
   }
-
-  $(window).scroll(function() {
-    drawLine( $('#route'),
-              $('#path') );
-  });
-  
-  // // init the line length
-  //   drawLine( $('#route'),
-  //             document.getElementById('path') );
-
-
   
 });
