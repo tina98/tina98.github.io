5cdb32b8cba42827f0af4a71dca1c8be196cb232
tina98 <tinali3698@gmail.com>
2014-06-17 12:02:52 -0400

    Update resumeJS.js

--- resume/js/resumeJS.js
+++ resume/js/resumeJS.js
@@ -23,25 +23,48 @@
 
   
 // });
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $(window).scroll(function() {
-    drawLine( $('#route'),
-              document.getElementById('path') );
-  });
+//   $(window).scroll(function() {
+//     drawLine( $('#route'),
+//               document.getElementById('path') );
+//   });
   
-  // init the line length
-    drawLine( $('#route'),
-              document.getElementById('path') );
+//   // init the line length
+//     drawLine( $('#route'),
+//               document.getElementById('path') );
 
-  //draw the line
-  function drawLine(container, line){
+//   //draw the line
+//   function drawLine(container, line){
     
-    var pathLength = line.getTotalLength(),
-        maxScrollTop = $(document).height() - $(window).height(),
-        percentDone = $(window).scrollTop() / maxScrollTop,
-        length = percentDone * pathLength;
-    line.style.strokeDasharray = [ length ,pathLength].join(' ');
-  }
+//     var pathLength = line.getTotalLength(),
+//         maxScrollTop = $(document).height() - $(window).height(),
+//         percentDone = $(window).scrollTop() / maxScrollTop,
+//         length = percentDone * pathLength;
+//     line.style.strokeDasharray = [ length ,pathLength].join(' ');
+//   }
   
+// });
+//On scroll call the draw function
+$(window).scroll(function() {
+    drawLines();
 });
+
+//If you have more than one SVG per page this will pick it up
+function drawLines(){
+    $.each($("path"), function(i, val){
+      var line = val;
+      drawLine($(this), line);
+    });
+}
+
+//draw the line
+function drawLine(container, line){
+  var length = 0;
+  var pathLength = line.getTotalLength();
+  var distanceFromTop = container.offset().top - $(window).scrollTop();
+  var percentDone = 1 - (distanceFromTop / $(window).height());
+  length = percentDone * pathLength;
+  line.style.strokeDasharray = [length,pathLength].join(' ');
+  console.log("strokeDasharray: "+[length,pathLength].join(' '));
+}
